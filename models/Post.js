083741266899995729da06db2c5820a9d1411076
muha <1student.muhammed1@gmail.com>
2024-04-21 00:24:58 +0300

    feat: set a virtual property for postedtime using luxon

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
+const luxon = require('luxon')
 
 const postSchema = new Schema({
     title: { type: String, require: true },
@@ -8,4 +9,7 @@ const postSchema = new Schema({
     user: { type: Schema.ObjectId, ref: 'User' }
 })
 
+postSchema.virtual('postedTime').get(function (req, res, next)  {
+    return luxon.DateTime.fromJSDate(this.timestamp).toLocaleString(luxon.DateTime.DATETIME_MED)
+})
 module.exports = mongoose.model('Post', postSchema)
